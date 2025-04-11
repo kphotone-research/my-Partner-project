46359107d2fd826070687c8233aebaf5ad712ab4
kphotone-research <kphotoneresearch@gmail.com>
2025-04-11 12:48:28 +0530

    Update add-supplier.ts

--- pages/api/add-supplier.ts
+++ pages/api/add-supplier.ts
@@ -1,1 +1,13 @@
-pages/api/add-supplier.ts
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { supplierName, supplierEmail } = req.body;
+
+    // For now, just return the data (you can later save this to Supabase)
+    res.status(200).json({ message: 'Supplier added', data: { supplierName, supplierEmail } });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
+
