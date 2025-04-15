e905dffa61189dfcdcd64c530dce5015a0c1bdea
kphotone-research <kphotoneresearch@gmail.com>
2025-04-15 15:49:52 +0530

    Update SupplierPage.js

--- src/SupplierPage.js
+++ src/SupplierPage.js
@@ -1,5 +1,13 @@
+// src/SupplierPage.js
 import React from 'react';
+
 function SupplierPage() {
-  return <h1>Welcome to the Supplier Page</h1>;
+  return (
+    <div>
+      <h2>Supplier Page</h2>
+      {/* Add supplier content or logic here */}
+    </div>
+  );
 }
+
 export default SupplierPage;
