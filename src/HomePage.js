3e289d34c7e84f9673807ec8e97f8aebcdc30c81
kphotone-research <kphotoneresearch@gmail.com>
2025-04-15 15:36:40 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 function HomePage() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to Kphotone Partner Redirect System</h1>
+      <p>This project handles survey redirects and supplier routing.</p>
+      
+      {/* Link to add a new project */}
+      <Link to="/add-project">
+        <button>Add New Project</button>
+      </Link>
+      
+      {/* Link to view all projects */}
+      <Link to="/projects">
+        <button>View All Projects</button>
+      </Link>
+    </div>
+  );
 }
+
 export default HomePage;
