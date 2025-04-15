70028e3ecb1766b724c390dbf10ff5a0386682c4
kphotone-research <kphotoneresearch@gmail.com>
2025-04-15 15:18:07 +0530

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://qbezfrqsojypxpmyblbc.supabase.co';
+const supabaseKey = 'GciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFiZXpmcnFzb2p5cHhwbXlibGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMjIxNDgsImV4cCI6MjA1OTU5ODE0OH0.FwfbMTlfPQ17OpoMXR3zDL13lwHwm3QlblV5dmHfEVc';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
